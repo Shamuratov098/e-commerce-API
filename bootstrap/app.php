f47683ca6f4ada04ab6baa728f3fc874d0b332b0
dostonbek <shamuratov1111@gmail.com>
2026-03-31 15:10:12 +0500

    Added EnsureAdminWeb middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureAdminWeb;
 use App\Http\Middleware\EnsureIsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin' => EnsureIsAdmin::class,
+            'admin.web' => EnsureAdminWeb::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
